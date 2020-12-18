ef0f0734113421a5fe3eec935d9d05c8aa195b04
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-18 13:45:42 -0500

    css created, app created and data created

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flat-list" />
+        <div className="map-container" />
+      </div>
+    );
+  }
+}
+
+export default App;
