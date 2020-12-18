16274c4cef53a7883986bb8016851b0382f7009a
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-18 15:07:28 -0500

    les flats sont affichés

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 
+import FlatList from "./flat_list";
+import flats from '../../data/flats';
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { flats };
   }
 
   render() {
     return (
       <div>
-        <div className="flat-list" />
+        <FlatList flats={this.state.flats} />
         <div className="map-container" />
       </div>
     );
---
+++ src/components/flat_list.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Flat from './flat';
+
+const FlatList = (props) => {
+  return (
+    <div className="flat-list">
+      { props.flats.map(flat => <Flat flat={flat} key={flat.name} />) }
+    </div>
+  );
+};
+
+export default FlatList;
