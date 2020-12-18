e5fca98b1c440af518176d4a3c01ec53c48a8d06
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-18 16:24:18 -0500

    not working really well

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
+import GoogleMapReact from 'google-map-react';
 
 import FlatList from "./flat_list";
 import flats from '../../data/flats';
+import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { flats };
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
   }
 
   render() {
     return (
       <div>
         <FlatList flats={this.state.flats} />
-        <div className="map-container" />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
 import App from "./components/app";
 
+
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<App />, root);
