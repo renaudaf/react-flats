57404f1143d0d1e80726819e4104884f6318977d
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-30 11:17:38 -0500

    refactoring done

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Flat extends Component {
-  handleClick = () => {
-    const { selectFlat, index } = this.props;
+const Flat = (props) => {
+  const handleClick = () => {
+    const { selectFlat, index } = props;
     selectFlat(index);
-  }
+  };
 
-  render() {
-    const { flat } = this.props;
-    return (
-      <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${flat.imageUrl}')` }}>
-        <div className="card-category">
-          {flat.price}
-          {' '}
-          {flat.priceCurrency}
-        </div>
-        <div className="card-description">
-          <h2>{flat.name}</h2>
-        </div>
-        <button type="button" className="card-link" href="#" onClick={this.handleClick} />
+  const { flat } = props;
+  return (
+    <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${flat.imageUrl}')` }}>
+      <div className="card-category">
+        {flat.price}
+        {' '}
+        {flat.priceCurrency}
       </div>
-    );
-  }
-}
+      <div className="card-description">
+        <h2>{flat.name}</h2>
+      </div>
+      <button type="button" className="card-link" href="#" onClick={handleClick} />
+    </div>
+  );
+};
 
 export default Flat;
